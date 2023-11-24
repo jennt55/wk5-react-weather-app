882afa967a9b973108d30d253496c22f088b04ea
jennt55 <146074617+jennt55@users.noreply.github.com>
2023-11-24 12:22:08 +0000

    added css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App container">
-      <h1>Weather App</h1>
+      <Weather />
       <footer>
         <a
           href="https://github.com/jennt55/wk5-react-weather-app"
---
+++ src/Weather.js
@@ -0,0 +1,51 @@
+import React from "react";
+import "./Weather.css";
+import "bootstrap/dist/css/bootstrap.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Search for a city..."
+              className="form-control"
+              autoFocus="on"
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
+          </div>
+        </div>
+      </form>
+      <h1>New York</h1>
+      <ul>
+        <li>Friday, 24th November, 2023</li>
+        <li>Sunny</li>
+      </ul>
+      <div className="row mt-3">
+        <div className="col-6">
+          <img
+            src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/098/762/original/sun.png?1696249142"
+            alt="sunny"
+          />
+          <span className="temperature">6</span>
+          <span className="unit">°C</span>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation:</li>
+            <li>Humidity:</li>
+            <li>Wind Speed:</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
