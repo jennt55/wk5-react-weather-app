2c66d88f06d0418d9dd0ae83279eccbea9c01bc5
jennt55 <146074617+jennt55@users.noreply.github.com>
2023-11-24 10:55:25 +0000

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App container">
+      <h1>Weather App</h1>
+      <footer>
+        <a
+          href="https://github.com/jennt55/wk5-react-weather-app"
+          target="_blank"
+        >
+          Open Source Code on Github
+        </a>{" "}
+        by Jenn Taft
+      </footer>
+    </div>
+  );
 }
